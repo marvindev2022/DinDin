6f06640f4769ae770bd90b8d6909f567bb7c2bce
Marcus Roza <marcusroza@pop-os.localdomain>
2023-06-03 19:57:44 -0300

    main.tsx refatorado

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import MainRoutes from "./routes";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <MainRoutes />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <MainRoutes />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
